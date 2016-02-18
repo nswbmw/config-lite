10cdc5e8932678a6f4aa224ba243f55f9fb6ea90
nswbmw <gxqzk@126.com>
2016-02-18 11:21:33 +0800

    exports origin errror & Bump 1.2.1

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var path = require('path');
 var fs   = require('fs');
 var resolve = require('resolve');
 var yaml = require('js-yaml');
@@ -12,17 +11,13 @@ var CONFIG_BASEDIR = process.env.CONFIG_BASEDIR || process.cwd();
 var CONFIG_DIR = process.env.CONFIG_DIR || 'config';
 var CONFIG = merge(JSON.parse(process.env.CONFIG || '{}'), argv);
 
-try {
-  var filepath = resolve.sync(id, {
-    basedir: CONFIG_BASEDIR,
-    extensions: ['.js', '.json', '.node', '.yaml', '.yml'],
-    moduleDirectory: CONFIG_DIR
-  });
-  if (/\.ya?ml$/.test(filepath)) {
-    module.exports = merge(CONFIG, yaml.safeLoad(fs.readFileSync(filepath , 'utf8')), false);
-  } else {
-    module.exports = merge(CONFIG, require(filepath), false);
-  }
-} catch (e) {
-  throw new Error('Cannot read config: ' + path.join(CONFIG_BASEDIR, CONFIG_DIR + '/' + id));
+var filepath = resolve.sync(id, {
+  basedir: CONFIG_BASEDIR,
+  extensions: ['.js', '.json', '.node', '.yaml', '.yml'],
+  moduleDirectory: CONFIG_DIR
+});
+if (/\.ya?ml$/.test(filepath)) {
+  module.exports = merge(CONFIG, yaml.safeLoad(fs.readFileSync(filepath , 'utf8')), false);
+} else {
+  module.exports = merge(CONFIG, require(filepath), false);
 }
